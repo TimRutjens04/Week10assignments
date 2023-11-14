f117177cdcb4f875bbdfba0748f84b2c7fadf96d
TimRutjens04 <150683348+TimRutjens04@users.noreply.github.com>
2023-11-14 12:49:44 +0100

    Add files of remaining exc

---
+++ Array-Exercise/Array-Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayExc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //......
+        }
+    }
+}
---
+++ NameExc/NameExc/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string Username = Console.ReadLine();
+            Console.WriteLine($"Hello {Username}");
+        }
+    }
+}
